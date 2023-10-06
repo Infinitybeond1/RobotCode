a2576ad2d065bd474246e1af2504b2997faaec8f
lonaslee <93890846+lonaslee@users.noreply.github.com>
2023-10-06 17:49:57 -0500

    feat: add kotlin support

---
+++ RobotCode-development/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadTest.java
@@ -0,0 +1,113 @@
+/* Copyright (c) 2021 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/*
+ * This file contains an example of a Linear "OpMode".
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When a selection is made from the menu, the corresponding OpMode is executed.
+ *
+ * This particular OpMode illustrates driving a 4-motor Omni-Directional (or Holonomic) robot.
+ * This code will work with either a Mecanum-Drive or an X-Drive train.
+ * Both of these drives are illustrated at https://gm0.org/en/latest/docs/robot-design/drivetrains/holonomic.html
+ * Note that a Mecanum drive must display an X roller-pattern when viewed from above.
+ *
+ * Also note that it is critical to set the correct rotation direction for each motor.  See details below.
+ *
+ * Holonomic drives provide the ability for the robot to move in three axes (directions) simultaneously.
+ * Each motion axis is controlled by one Joystick axis.
+ *
+ * 1) Axial:    Driving forward and backward               Left-joystick Forward/Backward
+ * 2) Lateral:  Strafing right and left                     Left-joystick Right and Left
+ * 3) Yaw:      Rotating Clockwise and counter clockwise    Right-joystick Right and Left
+ *
+ * This code is written assuming that the right-side motors need to be reversed for the robot to drive forward.
+ * When you first test your robot, if it moves backward when you push the left stick forward, then you must flip
+ * the direction of all 4 motors (see code below).
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name="Gamepad Test", group="Linear OpMode")
+//@Disabled
+public class GamepadTest extends LinearOpMode {
+
+    // Declare OpMode members for each of the 4 motors.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        while (opModeIsActive()) {
+            boolean a = gamepad2.a;
+            boolean b = gamepad2.b;
+            boolean x = gamepad2.x;
+            boolean y = gamepad2.y;
+            boolean left_js = gamepad2.left_stick_button;
+            boolean right_js = gamepad2.right_stick_button;
+            boolean dp_up = gamepad2.dpad_up;
+            boolean dp_down = gamepad2.dpad_down;
+            boolean dp_left = gamepad2.dpad_left;
+            boolean dp_right = gamepad2.dpad_right;
+            float lt = gamepad2.left_trigger;
+            float rt = gamepad2.right_trigger;
+            boolean lb = gamepad2.left_bumper;
+            boolean rb = gamepad2.right_bumper;
+
+            telemetry.addData("Keys a", a);
+            telemetry.addData("Keys b", b);
+            telemetry.addData("Keys x", x);
+            telemetry.addData("Keys y", y);
+            telemetry.addData("Left Stick", left_js);
+            telemetry.addData("Right Stick", right_js);
+            telemetry.addData("Hat Up", dp_up);
+            telemetry.addData("Hat Down", dp_down);
+            telemetry.addData("Hat Left", dp_left);
+            telemetry.addData("Hat Right", dp_right);
+            telemetry.addData("Left Trigger", lt);
+            telemetry.addData("Right Trigger", rt);
+            telemetry.addData("Left Bumper", lb);
+            telemetry.addData("Right Bumper", rb);
+
+            telemetry.update();
+        }
+    }}
---
+++ RobotCode-development/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/moveMotor.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "Move Motor", group = "Concept")
+//@Disabled
+
+public class moveMotor extends LinearOpMode{
+
+    private DcMotor m1 = null;
+
+    public void runOpMode(){
+        m1 = hardwareMap.get(com.qualcomm.robotcore.hardware.DcMotor.class, "m1");
+
+        telemetry.addData(">", "Press start gangy" );
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double leftY1 = gamepad1.left_stick_y;
+            double rightY1 = gamepad1.right_stick_y;
+            double leftX1 = gamepad1.left_stick_x;
+            double rightX1 = gamepad1.right_stick_x;
+            double leftY2 = gamepad2.left_stick_y;
+            double rightY2 = gamepad2.right_stick_y;
+            double leftX2 = gamepad2.left_stick_x;
+            double rightX2 = gamepad2.right_stick_x;
+            boolean a_down = gamepad1.a;
+            boolean b_down = gamepad1.b;
+
+            //m1.setPower(1);
+            //sleep();
+
+            if (a_down) {
+                m1.setPower(rightY1);
+            }else if (b_down) {
+                m1.setPower(-.5);
+            } else {m1.setPower(0);}
+
+            telemetry.addData(">", a_down + " left: " + leftX2 + ", " + leftY2 + "  ||| right: " + rightX2 + ", " + rightY2 );
+            telemetry.update();
+
+        }
+
+    }
+}
